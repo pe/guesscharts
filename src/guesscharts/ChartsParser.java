ccd4cce4e332515e535f5412136fac68b6e6cf0a
Philippe Eberli <p.eberli@gmx.ch>
2015-08-02 12:32:48 +0200

    Fix off by one.

--- src/guesscharts/ChartsParser.java
+++ src/guesscharts/ChartsParser.java
@@ -19,7 +19,7 @@ public abstract class ChartsParser<T extends ChartEntry> {
 	public void nextSong(int yearFrom, int yearTo, int positionFrom, int positionTo) {
 		// Some subclasses require numberOfChartEntriesOfYear to be called before the *OfPosition methods.
 		int year = randomInt(yearFrom, yearTo);
-		int position = randomInt(positionFrom, Math.min(positionTo, numberOfChartEntries(year)));
+		int position = randomInt(positionFrom, Math.min(positionTo, numberOfChartEntries(year))) - 1;
 
 		chartEntry.setYear(year);
 		chartEntry.setPosition(position);
