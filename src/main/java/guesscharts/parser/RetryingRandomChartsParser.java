31afeee6e341bd24a0465d6725cf0e4d3729acc6
Philippe Eberli <p.eberli@gmx.ch>
2019-04-13 21:35:56 +0200

    Extracted retry-logic to own class

--- src/main/java/guesscharts/parser/RetryingRandomChartsParser.java
+++ src/main/java/guesscharts/parser/RetryingRandomChartsParser.java
@@ -2,6 +2,7 @@ package guesscharts.parser;
 
 import guesscharts.ParsingError;
 import guesscharts.parser.util.Random;
+import guesscharts.parser.util.Retry;
 
 import java.io.IOException;
 
@@ -9,42 +10,25 @@ import java.io.IOException;
  * Tries loading random chart entries and retries on errors.
  */
 public class RetryingRandomChartsParser {
+	public static final int MAX_RETRY_ATTEMPTS = 10;
+
 	private final ChartsParser parser;
-	private final int maxTries;
-	private final Random random;
+	private final Random random = new Random();
+	private final Retry retry = new Retry(MAX_RETRY_ATTEMPTS);
 
 	public RetryingRandomChartsParser(ChartsParser parser) {
-		this(parser, 10);
-	}
-
-	public RetryingRandomChartsParser(ChartsParser parser, int tries) {
-		if (tries < 1) {
-			throw new IllegalArgumentException();
-		}
 		this.parser = parser;
-		this.maxTries = tries;
-		this.random = new Random();
 	}
 
 	/**
 	 * Tries to load a random {@link ChartsEntry} within the given parameters. Retry
-	 * with different random values if a {@link ParsingError} or {@link IOException}
-	 * is thrown.
+	 * with different random values if a {@link ParsingError} or {@link IOException} is thrown.
 	 */
 	public ChartsEntry getRandomEntry(int yearFrom, int yearTo, int positionFrom, int positionTo) {
-		for (int tryCount = 1; tryCount <= maxTries; tryCount++) {
-			try {
-				int position = random.between(positionFrom, positionTo);
-				int year = random.between(yearFrom, yearTo);
-				return parser.getEntry(year, position);
-			} catch (ParsingError | IOException e) {
-				if (tryCount < maxTries) {
-					System.out.println(e.toString() + " retrying...");
-				} else {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-		throw new IllegalStateException();
+		return retry.run(() -> {
+			int position = random.between(positionFrom, positionTo);
+			int year = random.between(yearFrom, yearTo);
+			return parser.getEntry(year, position);
+		});
 	}
 }
---
+++ src/test/java/guesscharts/parser/util/RetryTest.java
@@ -0,0 +1,69 @@
+package guesscharts.parser.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class RetryTest {
+	@Test
+	void exceptionThrownWhenMaxAttemptsSmallerThan1() {
+		int maxAttempts = 0;
+
+		assertThrows(IllegalArgumentException.class, () -> new Retry(maxAttempts));
+	}
+
+	@Test
+	void returningEntryFromUnderlyingCallable() {
+		int maxAttempts = 1;
+		String expected = "return value";
+		Callable<String> task = () -> expected;
+
+		String actual = new Retry(maxAttempts).run(task);
+
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	void exceptionOfFirstCallSuppressedWhenTryingTwice() {
+		int maxAttempts = 2;
+		Callable<Void> throwException = ThrowException.forNCalls(1);
+
+		assertDoesNotThrow(() -> new Retry(maxAttempts).run(throwException));
+	}
+
+	@Test
+	void exceptionOfSecondCallThrownWhenTryingTwice() {
+		int maxAttempts = 2;
+		Callable<Void> throwException = ThrowException.forNCalls(2);
+
+		assertThrows(Exception.class, () -> new Retry(maxAttempts).run(throwException));
+	}
+
+	private static class ThrowException implements Callable<Void> {
+		private final int numberOfCalls;
+		private int callCount = 0;
+
+		static ThrowException forNCalls(int numberOfCalls) {
+			return new ThrowException(numberOfCalls);
+		}
+
+		private ThrowException(int numberOfCalls) {
+			this.numberOfCalls = numberOfCalls;
+		}
+
+		@Override
+		public Void call() throws Exception {
+			callCount++;
+			if (callCount <= numberOfCalls) {
+				throw new Exception();
+			} else {
+				return null;
+			}
+		}
+	}
+}
