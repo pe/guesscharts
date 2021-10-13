22daf7520a4adacd00a99571448fefd2dbbb696a
Philippe Eberli <p.eberli@gmx.ch>
2021-10-13 19:59:31 +0200

    Upgrade to Java15 and cleanup gradle scripts

--- fx/src/main/java/module-info.java
+++ fx/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module guesscharts.fx {
-	requires javafx.base;
 	requires javafx.controls;
-	requires javafx.graphics;
 	requires javafx.media;
 	requires guesscharts.domain;
 	opens guesscharts.java_fx to javafx.graphics;
