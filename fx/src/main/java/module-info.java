571bb2852814c9bcb9109f6e4759b9b4d0605c30
Philippe Eberli <p.eberli@gmx.ch>
2025-05-26 20:21:05 +0200

    Add jspecify

--- fx/src/main/java/module-info.java
+++ fx/src/main/java/module-info.java
@@ -1,6 +1,10 @@
+import org.jspecify.annotations.NullMarked;
+
+@NullMarked
 module guesscharts.fx {
     requires javafx.controls;
     requires javafx.media;
     requires guesscharts.domain;
+    requires org.jspecify;
     opens guesscharts.java_fx to javafx.graphics;
 }
